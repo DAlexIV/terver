607a494138962bb2854051bf98064dfbd6e93a19
DAlexIV <dalexiv@yandex.ru>
2015-12-12 20:29:35 +0300

    Release

--- terver.py
+++ terver.py
@@ -4,14 +4,20 @@ import pandas as pd
 import numpy as np
 import math
 
-start = 201
-finish = 600
-n = finish - start + 1
-table = [1.65, 1.70, 1.75, 1.81, 1.88, 1.96, 2.06, 2.1, 2.33, 2.58, 3.8]
-main_value_from_table = table[5]
-MAX = 100000000
+start = 1801 # First value of dataset
+finish = 2200 # First value of dataset
+n = finish - start + 1 # Number of entries in dataset
 
+# Values from table
+table_norm_left = [1.65, 1.70, 1.75, 1.81, 1.88, 1.96, 2.06, 2.1, 2.33, 2.58, 3.8]
+table_norm_right = list(table_norm_left) # Values reversed
+table_norm_right.reverse()
 
+main_value_from_table = 5 # Value for distribution in the second task
+table_student = 1.9791241 # Value for mean in the second task
+MAX = 100000000 # Just max value
+
+# Counts simple series variance
 def getVariance(series):
     sum = 0
     for value in series:
@@ -22,36 +28,41 @@ def getVariance(series):
 def doFirstTask():
     print("First task:")
 
+    # Creating new dataset which contains only people who wait more than year
     more_than_year_df = my_df[my_df['SearchTime'] == 5]
-    more_than_year_df.to_csv("output_data/output_data_my.csv", sep=',', encoding='utf-8')
-    perc = float(more_than_year_df.shape[0]) / n
+    more_than_year_df.to_csv("output_data/my_data_1_task.csv", sep=',', encoding='utf-8')
+    perc = float(more_than_year_df.shape[0]) / n # These people portion
 
-    left_border = perc - main_value_from_table * math.sqrt((perc * (1 - perc)) / n)
-    print("Left border of confidence interval " + str(left_border))
+    left_border = perc - table_norm_left[main_value_from_table] * math.sqrt((perc * (1 - perc)) / n)
+    print("Left border of portion confidence interval " + str(left_border))
 
-    right_border = perc + main_value_from_table * math.sqrt((perc * (1 - perc)) / n)
-    print("Right border of confidence interval " + str(right_border))
+    right_border = perc + table_norm_left[main_value_from_table] * math.sqrt((perc * (1 - perc)) / n)
+    print("Right border of portion confidence interval " + str(right_border))
 
     print("")
 
-
-
-def countBordersVariance(table_value, series):
-    left = (len(series) - 1) * getVariance(series) / (0.5 * math.pow((table_value + math.sqrt(2 * (len(series) - 1))), 2))
-    right = (len(series) - 1) * getVariance(series) / (0.5 * math.pow((-table_value + math.sqrt(2 * (len(series) - 1))), 2))
+# Counts borders for confidence interval for variance
+# with given percentage from the table
+def countBordersVariance(i, series):
+    local_n = len(series) - 1
+    left = local_n * getVariance(series) / (0.5 * math.pow((table_norm_left[i] + math.sqrt(2 * local_n - 1)), 2))
+    right = local_n * getVariance(series) / (0.5 * math.pow((-table_norm_right[i] + math.sqrt(2 * local_n - 1)), 2))
     return left, right
 
-
+# Counts borders for confidence interval for mean with 90%
 def countBordersMean(series):
-    left = series.mean() - main_value_from_table * math.sqrt(getVariance(series) / len(series))
-    right = series.mean() + main_value_from_table * math.sqrt(getVariance(series) / len(series))
+    left = series.mean() - table_student * math.sqrt(getVariance(series) / len(series))
+    right = series.mean() + table_student * math.sqrt(getVariance(series) / len(series))
     return left, right
 
 
 def doSecondTask():
     print("Second task:")
 
+    # Choosing only not null values
     known_value_df = my_df[my_df["Benefits"].notnull()]
+    known_value_df.to_csv("output_data/my_data_2_task.csv", sep=',', encoding='utf-8')
+
     left_mean, right_mean = countBordersMean(known_value_df["Benefits"])
 
     print("Mean for dataset is " + str(known_value_df["Benefits"].mean()))
@@ -72,33 +83,33 @@ def doThirdTask():
     print("Third task:")
     # Do the preprocessing
     known_value_df = my_df[my_df["Benefits"].notnull()]
-    known_value_df.to_csv("output_data/output_data_my_misha.csv", sep=',', encoding='utf-8')
-    less_than_half_df = known_value_df[my_df['SearchTime'] <= 3]
+    less_than_half_df = known_value_df[known_value_df['SearchTime'] <= 3]
+    less_than_half_df.to_csv("output_data/my_data_3_task.csv", sep=',', encoding='utf-8')
 
+    best_i = 0
     min_dist = MAX
-    cached_left = 0
-    cached_right = 0
-    cached_i = 0
-
-    for i, table_value in enumerate(table):
-        temp_left, temp_right = countBordersVariance(table_value, less_than_half_df["Benefits"])
+    best_left = 0
+    best_right = 0
+    # Running over all possible situations
+    for i in range(1, len(table_norm_left) - 1):
+        temp_left, temp_right = countBordersVariance(i, less_than_half_df["Benefits"])
         cur = temp_right - temp_left
 
         if (cur < min_dist):
-            cached_i = i
+            best_i = i
             min_dist = cur
-            cached_left = temp_left
-            cached_right = temp_right
+            best_left = temp_left
+            best_right = temp_right
 
-    print("Best confidence interval is from " + str(cached_i) + " to " + str(cached_i + 90))
-    print("Left border of confidence interval for distribution " + str(cached_left))
-    print("Right border of confidence interval for distribution " + str(cached_right))
+    print("Best confidence interval is from " + str(best_i) + " to " + str(best_i + 90))
+    print("Left border of confidence interval for distribution " + str(best_left))
+    print("Right border of confidence interval for distribution " + str(best_right))
     print("Minimum distance is " + str(min_dist))
     print("")
 
 
-
 train_df = pd.read_csv("input_data/input_data.csv")
+# Only my vaues in dataset
 my_df = train_df[(train_df.Obs >= start) & (train_df.Obs <= finish)]
 
 doFirstTask()
